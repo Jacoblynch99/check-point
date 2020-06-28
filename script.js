e43a0b6a58188a2fbac194151114423b8ddf9ce8
Jacoblynch99 <jacoblynch99@gmail.com>
2020-06-28 09:44:15 -0500

    massive overhaul

---
+++ script.js
@@ -0,0 +1,8 @@
+function handleClick() {
+    let x = document.getElementById("open");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
